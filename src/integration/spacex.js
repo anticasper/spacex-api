9f82685250a4d3b27eae7c87c6abf07f5b38f143
Diovanni Souza <diovanni@anticasper.com>
2022-12-19 14:53:44 -0300

    feat: create base spacex file

---
+++ src/integration/spacex.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+const baseApiUrl = 'https://api.spacexdata.com/v4'
+
+const getLastLaunch = async () => {}
+
+const getPastLaunch = async () => {}
+
+const getNextLaunch = async () => {}
+
+const getUpcommingLaunch = async () => {}
