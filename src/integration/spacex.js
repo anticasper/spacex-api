2a64f49c6edcf369d66d2534a53091480b0ef2f4
Diovanni Souza <diovanni@anticasper.com>
2022-12-19 14:58:37 -0300

    feat: add methods

--- src/integration/spacex.js
+++ src/integration/spacex.js
@@ -1,10 +1,35 @@
 const axios = require('axios')
 const baseApiUrl = 'https://api.spacexdata.com/v4'
 
-const getLastLaunch = async () => {}
+const getGeneric = async (route) => {
+  return await axios.get(route).catch((err) => {
+    return {
+      error: {
+        code: err.code,
+        message: err.message,
+      },
+    }
+  })
+}
 
-const getPastLaunch = async () => {}
+const getLastLaunch = async () => {
+  const url = `${baseApiUrl}/launches/latest`
+  return await getGeneric(url)
+}
 
-const getNextLaunch = async () => {}
+const getPastLaunch = async () => {
+  const url = `${baseApiUrl}/launches/past`
+  return await getGeneric(url)
+}
 
-const getUpcommingLaunch = async () => {}
+const getNextLaunch = async () => {
+  const url = `${baseApiUrl}/launches/next`
+  return await getGeneric(url)
+}
+
+const getUpcommingLaunch = async () => {
+  const url = `${baseApiUrl}/launches/upcoming`
+  return await getGeneric(url)
+}
+
+module.exports = { getLastLaunch, getPastLaunch, getNextLaunch, getUpcommingLaunch }
